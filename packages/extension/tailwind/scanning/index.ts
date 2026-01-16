169ecd08097fb35120c766cb5a7e6914897b75c3
Garrett Fincke <garrettfincke@gmail.com>
2026-01-15 19:14:30 -0500

    refactor(tailwind): extract scanning into dedicated modules

---
+++ packages/extension/tailwind/scanning/index.ts
@@ -0,0 +1,7 @@
+// packages/extension/tailwind/scanning/index.ts
+// barrel exports for the Tailwind scanning subsystem
+
+export * from './types';
+export { PatternScanner } from './PatternScanner';
+export { ContentScanner } from './ContentScanner';
+export { DependencyScanner } from './DependencyScanner';
