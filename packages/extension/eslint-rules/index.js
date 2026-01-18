d40d3918425c6980c2255c0db147d6e7fd7ea5aa
Garrett Fincke <garrettfincke@gmail.com>
2026-01-18 16:20:53 -0500

    feat(lint): add custom eslint rule to enforce ConfigManager usage

---
+++ packages/extension/eslint-rules/index.js
@@ -0,0 +1,8 @@
+// packages/extension/eslint-rules/index.js
+// Local ESLint plugin for mdx-preview extension rules
+
+module.exports = {
+  rules: {
+    'no-direct-vscode-config': require('./no-direct-vscode-config'),
+  },
+};
