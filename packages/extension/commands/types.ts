6aadd7fef1f3da10c02ed0ec3c1843830c733ca6
Garrett Fincke <garrettfincke@gmail.com>
2026-01-18 22:41:00 -0500

    build: update dependencies and tsconfig for new package structure

--- packages/extension/commands/types.ts
+++ packages/extension/commands/types.ts
@@ -1,8 +1,6 @@
 // packages/extension/commands/types.ts
 // command handler types and interfaces
 
-import type * as vscode from 'vscode';
-
 // Command handler function signature.
 // Commands can be sync or async, w/ no arguments.
 export type CommandHandler = () => void | Promise<void>;
@@ -15,13 +13,3 @@ export interface CommandDefinition {
   handler: CommandHandler;
 }
 
-// Command module interface - each command file exports this.
-export interface CommandModule {
-  // Array of command definitions to register
-  commands: CommandDefinition[];
-}
-
-// Utility type for QuickPick items w/ extra data.
-export interface QuickPickItemWithValue<T> extends vscode.QuickPickItem {
-  value: T;
-}
