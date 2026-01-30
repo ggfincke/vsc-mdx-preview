875a895aa85d1e3c913980538bf04cf0682348db
Garrett Fincke <garrettfincke@gmail.com>
2026-01-29 22:55:42 -0500

    feat(shared): add logging types & centralized log tags

---
+++ packages/shared/logging/index.ts
@@ -0,0 +1,14 @@
+// packages/shared/logging/index.ts
+// barrel export for shared logging types & tags
+
+export {
+  LogLevel,
+  type LogFn,
+  type LogFnVariadic,
+  type Logger,
+  type LoggerVariadic,
+  type TaggedLogger,
+  type TaggedLoggerFactory,
+} from './types';
+
+export { LogTags, type LogTag } from './tags';
