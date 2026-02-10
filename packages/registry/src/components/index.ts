7caf8fc26068e1fd801c24bd26bc4b64c96ee23c
Garrett Fincke <ggf5038@psu.edu>
2026-02-10 09:22:06 -0500

    refactor: delete packages/shared, registry components/shims & shared-compat artifacts

--- packages/registry/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// packages/registry/src/components/index.ts
-// barrel exports for component registry
-
-export * from './types';
-export * from './registry-data';
-export * from './queries';
--- packages/registry/src/shims/index.ts
+++
@@ -1,4 +0,0 @@
-// packages/registry/src/shims/index.ts
-// shim manifest exports for code generation
-
-export * from './shim-config';
