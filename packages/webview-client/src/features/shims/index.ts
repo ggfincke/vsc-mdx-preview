3637187f49eeb3ae4d6e3dc8c7e223a8978ab120
Garrett Fincke <ggf5038@psu.edu>
2026-02-11 11:24:49 -0500

    refactor(webview): delete shim wrapper layer, use generated barrels directly

--- packages/webview-client/src/features/shims/index.ts
+++
@@ -1,8 +0,0 @@
-// wrapper-only import surface for generated shim barrels
-import './vscode-dark-adapter.css';
-
-export * as generic from './generic';
-export * as docusaurus from './docusaurus';
-export * as starlight from './starlight';
-export * as nextra from './nextra';
-export * as nextjs from './nextjs';
